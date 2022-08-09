d329e30a55d1c64c77d024d10be578c210a214c7
Connor Mackay <connor.mackay@gmail.com>
2022-08-09 14:55:27 -0400

    IMP: build and import Header Component, style.

---
+++ src/components/modules/Header/Header.js
@@ -0,0 +1,16 @@
+// Framework Imports
+import React from 'react'
+
+// CSS Imports
+
+import StyledHeader from './StyledHeader'
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <p>I am Header</p>
+    </StyledHeader>
+  )
+}
+
+export default Header
---
+++ src/components/modules/Header/StyledHeader.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const StyledHeader = styled.section`
+background-color: red;
+width: 100%;
+border: 1px solid pink;
+`
+
+export default StyledHeader
