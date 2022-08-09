92ccba1cc32b5fb666f33a3b60fb846ad655e841
Connor Mackay <connor.mackay@gmail.com>
2022-08-09 15:09:24 -0400

    CNF: configure webpack and eslint to allow aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,42 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react'],
+  plugins: ["react"],
   rules: {
-    semi: ['error', 'never'],
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    indent: ['error', 2],
-    'arrow-body-style': ['error', 'always'],
+    semi: ["error", "never"],
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    indent: ["error", 2],
+    "arrow-body-style": ["error", "always"],
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      alias: {
+        map: [
+          [src, "./src"],
+          [public, "./public"],
+          [elements, "./src/components/elements"],
+          [helpers, "./src/components/helpers"],
+          [modules, "./src/components/modules"],
+          [pages, "./src/components/pages"],
+          [templates, "./src/components/templates"],
+        ],
+        extensions: [".ts", ".js", ".jsx", ".json"],
+      },
+    },
+  },
+};
