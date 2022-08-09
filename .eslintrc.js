4d0449c11ae2deabe2495edd1aeef4c2b78a4187
Connor Mackay <connor.mackay@gmail.com>
2022-08-09 15:19:07 -0400

    CNF: Configure Aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,42 +3,40 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    semi: ["error", "never"],
-    "react/function-component-definition": [
+    semi: ['error', 'never'],
+    'react/function-component-definition': [
       2,
-      { namedComponents: "arrow-function" },
+      { namedComponents: 'arrow-function' },
     ],
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    indent: ["error", 2],
-    "arrow-body-style": ["error", "always"],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    indent: ['error', 2],
+    'arrow-body-style': ['error', 'always'],
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
-    "import/resolver": {
+    'import/resolver': {
       alias: {
         map: [
-          [src, "./src"],
-          [public, "./public"],
-          [elements, "./src/components/elements"],
-          [helpers, "./src/components/helpers"],
-          [modules, "./src/components/modules"],
-          [pages, "./src/components/pages"],
-          [templates, "./src/components/templates"],
+          ['src', './src'],
+          ['public', './public'],
+          ['elements', './src/components/elements'],
+          ['modules', './src/components/modules'],
+          ['layouts', './src/components/layouts'],
         ],
-        extensions: [".ts", ".js", ".jsx", ".json"],
+        extensions: ['.ts', '.js', '.jsx', '.json'],
       },
     },
   },
-};
+}
